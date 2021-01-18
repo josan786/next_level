b6908c692837b5c8ee5f78edb29a0d6b23a2d348
Ave_Kapusta <k.starikov@lenta.ru>
2021-01-18 23:31:50 +0400

    Fixed a bug in the names of elements in activity_login registrationLoginText -> loginText registrationPasswordText -> passwordText

--- IzhHelper/app/src/main/java/ru/konstantin_starikov/samsung/izhhelper/MainActivity.java
+++ IzhHelper/app/src/main/java/ru/konstantin_starikov/samsung/izhhelper/MainActivity.java
@@ -25,8 +25,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        loginText = (TextView) findViewById(R.id.registrationLoginText);
-        passwordText = (TextView) findViewById(R.id.registrationPasswordText);
+        loginText = (TextView) findViewById(R.id.loginText);
+        passwordText = (TextView) findViewById(R.id.passwordText);
     }
 
     public void LogIntoAccount(View view)
