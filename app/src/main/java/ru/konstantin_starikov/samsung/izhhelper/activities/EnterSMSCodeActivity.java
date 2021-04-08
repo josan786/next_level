0a553e5a42a43eb7fac1ae7325324f995cab7b5e
Konstantin Starikov <k.starikov@lenta.ru>
2021-04-08 19:30:34 +0400

    Start adding authentication with phone number
    Added activities:
    EnterSMSCodeActivity
    LoginWithPhoneNumberActivity
    
    !Application in this assembly doesn't work

---
+++ app/src/main/java/ru/konstantin_starikov/samsung/izhhelper/activities/EnterSMSCodeActivity.java
@@ -0,0 +1,16 @@
+package ru.konstantin_starikov.samsung.izhhelper.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ru.konstantin_starikov.samsung.izhhelper.R;
+
+public class EnterSMSCodeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_s_m_s_code);
+    }
+}
